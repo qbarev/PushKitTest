5420d5cc485ba49a389fcb0a9c6768b0cc8962d3
Kirill Kubarev <kirill.kubareff@gmail.com>
2022-11-08 18:29:45 +0300

    Initial Commit

---
+++ PushKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PushKitTest
+//
+//  Created by Кубарев Кирилл Юрьевич on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
